ccb4c0978ec3379a1e663699cda0f9d85f0ac754
empodi <kglein9513@gmail.com>
2022-01-11 18:46:11 +0900

    Update App.js - list movies

--- src/App.js
+++ src/App.js
@@ -3,12 +3,56 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back to React</h1>
-      <Button text={"Continue"} />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres &&
+                  movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+/* same as getMovies function 
+  useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+*/
